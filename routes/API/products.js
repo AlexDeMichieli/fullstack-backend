9c6a78cd4eedfb475aa6e59b9f8223682ae765bb
alexdemichieli <alexdemichieli@github.com>
2024-06-26 16:09:52 -0700

    adding products apis

--- routes/API/products.js
+++ routes/API/products.js
@@ -15,9 +15,65 @@ router.get("/products", async (req, res) => {
   }
 });
 
-router.get("/test", async (req, res) => {
-  res.json({ message: "hello there" });
+router.get('/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await prisma.product.findUnique({
+            where: {
+                id: parseInt(id)
+            }
+        })
+        if (!product) {
+            return res.status(404).json({error: "Product not found"})
+        }
+        res.status(200).json(product)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: "Internal Server Error"})
+    }
 })
 
+router.delete('/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await prisma.product.delete({
+            where: {
+                id: parseInt(id)
+            }
+        })
+        if (!product) {
+            return res.status(404).json({error: "Product not found"})
+        }
+        res.status(200).json({"message": "Product deleted"})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: "Internal Server Error"})
+    }
+})
+
+router.patch('/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, description, price, quantity } = req.body;
+        const product = await prisma.product.update({
+            where: {
+                id: parseInt(id)
+            },
+            data: {
+                name,
+                description,
+                price,
+                quantity
+            }
+        })
+        if (!product) {
+            return res.status(404).json({error: "Product not found"})
+        }
+        res.status(201).json(product)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: "Internal Server Error"})
+    }
+})
 
 module.exports = router;
