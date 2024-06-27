c65b0bbb476ece1c3e0eabf5a453da002a4802b2
alexdemichieli <alexdemichieli@github.com>
2024-06-27 16:27:40 -0700

    adding cors

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 const applyRoutes = require('./routes');
 
-
+app.use(cors())
 app.use(express.json());
 
 applyRoutes(app);
